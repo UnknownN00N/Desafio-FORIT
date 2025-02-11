2a676960337f52568a99df5506c9c3edf3c0eb31
= <=>
2025-02-11 08:46:17 -0300

    initial files

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const path = require('path');
+const PORT = 3000;
+const app = express();
+
+const indexRouter = require("./routes/index.routes.js");
+
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app
+.use('/', indexRouter)
+
+
+
+
+app.listen(PORT, () =>
+console.log("Server is running in: " + "http://localhost:" + PORT));
